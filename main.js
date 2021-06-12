f0cd253d481394bee365a854a778b9ca71154e0c
chili0118 <b10236181@gapps.uch.edu.tw>
2021-06-12 23:23:20 +0800

    更新banner

--- main.js
+++ main.js
@@ -73,3 +73,27 @@ $(".box-close").click(function () {
 });
 
 
+// 往下捲動
+
+(function() {
+    'use strict';
+  
+    var btnScrollDown = document.querySelector('#scroll_down');
+  
+    function scrollDown() {
+      var windowCoords = document.documentElement.clientHeight;
+      (function scroll() {
+        if (window.pageYOffset < windowCoords) {
+          window.scrollBy(0, 10);
+          setTimeout(scroll, 0);
+        }
+        if (window.pageYOffset > windowCoords) {
+          window.scrollTo(0, windowCoords);
+        }
+      })();
+    }
+  
+    btnScrollDown.addEventListener('click', scrollDown);
+  })();
+
+
