7a840c53a8d6d5d23400740cbf7d459abbeda3b7
chili0118 <b10236181@gapps.uch.edu.tw>
2021-07-02 21:40:42 +0800

    20210702更新房型版面及設定JS

--- main.js
+++ main.js
@@ -12,8 +12,10 @@ $("#close").click(function () {
     $(".navbox").fadeOut();
 });
 
+
+// 首頁按鈕設定
 var index = 0;
-var prevBtn = document.querySelector(".prev-btn")
+var prevBtn = document.querySelector(".prev-btn");
 // console.log(prevBtn);
 var nextBtn = document.querySelector(".next-btn");
 // console.log(nextBtn);
@@ -24,13 +26,69 @@ var smallimg = document.getElementsByClassName("simg");
 var textarea = document.getElementsByClassName("land-text");
 
 
+var r_nextBtn = document.querySelector("#rooms-next");
+// console.log(r_nextBtn);
+var r_prevBtn = document.querySelector("#rooms-prev");
+// console.log(r_prevBtn);
+var r_content = document.getElementsByClassName("r-i-content");
+// console.log(r_content.length);
+var r_title = document.getElementsByClassName("r-m-title");
+// console.log(r_title);
+
+function showTitle() {
+    for (let r = 0; r < r_title.length; r++) {
+        r_title[r].classList.remove("r-t-active");
+    }
+    r_title[index].classList.add("r-t-active");
+
+};
+
+function clickTitle(r) {
+    index = r;
+    show_r_content();
+    showTitle();
+}
+
+for (let r = 0; r < r_title.length; r++) {
+    r_title[r].onclick = function() {
+        clickTitle(r);
+    };
+}
+
+function r_next() {
+    index++;
+    // console.log(+index);
+    if (index == r_content.length) index = 0;
+    show_r_content();
+    showTitle();
+};
+
+function r_prev() {
+    index--;
+    if (index == -1) index = r_content.length -1;
+    show_r_content();
+    showTitle();
+}
+
+function show_r_content() {
+    for (let r = 0; r < r_content.length; r++) {
+        r_content[r].classList.remove("rooms-active");
+    }
+    r_content[index].classList.add("rooms-active");
+};
+
+r_nextBtn.onclick = r_next;
+r_prevBtn.onclick = r_prev;
+
+
+// 首頁按鈕設定
 function prev() {
     index--;
     if (index == -1) index = bigimg.length -1;
     showbimg();
     showsimg();
     showtext();
-}
+};
 
 
 function next() {
@@ -43,8 +101,10 @@ function next() {
     showtext();
 };
 
+
 prevBtn.onclick = prev;
 nextBtn.onclick = next;
+
 
 function showbimg() {
     for (let i = 0; i < bigimg.length; i++) {
@@ -58,19 +118,24 @@ function showsimg() {
         smallimg[s].classList.remove("simg-active");
     }
     smallimg[index].classList.add("simg-active");
-}
+};
 
 function showtext() {
     for (let t = 0; t < textarea.length; t++) {
         textarea[t].classList.remove("text-active");
     }
     textarea[index].classList.add("text-active");
-}
+};
 
 
+
+
+// 公告關閉按鈕設定
 $(".box-close").click(function () {
     $("#news-box").hide();
 });
+
+
 
 
 // 往下捲動
